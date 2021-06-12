169ad53d570e895e996de79102854111dee30ea5
Giovani Lamastra <giovanilamastra@gmail.com>
2021-06-12 00:54:59 -0300

    verrcel

--- src/components/HomeScreenLeft.js
+++ src/components/HomeScreenLeft.js
@@ -13,66 +13,70 @@ const HomeScreenLeft = ({ data }) => {
 
    return (
       <div>
-         {isMenuOpen ? (
-            <HomeScreenLeftMenu setIsMenuOpen={setIsMenuOpen} />
-         ) : (
-            <div className="px-4 md:px-1 lg:px-7 xl:px-10 2xl:px-14 pt-6  md:w-full z-0  lex flex-col overflow-x-hidden bg-purple100  min-h-screen">
-               <header className="flex relative z-10 justify-between p-4">
-                  <input
-                     className="px-4 duration-200 cursor-pointer filter hover:bg-gray-700 outline-none py-2 shadow-md text-gray-50 bg-gray-500"
-                     onClick={() => setIsMenuOpen(true)}
-                     type="button"
-                     value="Search for places"
-                  />
-                  <MdGpsFixed
-                     color="white"
-                     onClick={() => setIsMenuOpen(true)}
-                     className="hover:bg-gray-700 duration-200 bg-gray-500 cursor-pointer text-gray-50 text-4xl p-2 box-border rounded-full"
-                  />
-               </header>
-               <div className="relative h-80 mt-4      ">
-                  <div className="w-bg absolute left-1/2 top-2/4 transform-translate">
-                     <img
-                        className="  opacity-10  w-bg   h-full    overflow-x-hidden  "
-                        src="/images/Cloud-background.png"
-                        alt=" a"
-                     />
-                  </div>
-                  {data.data.consolidated_weather && (
-                     <img
-                        className="absolute left-2/4 top-2/4 transform-translate  max-w-1 w-40"
-                        src={`/images/${data.data.consolidated_weather[0].weather_state_name.replace(
-                           " ",
-                           ""
-                        )}.png`}
-                        alt=""
-                     />
-                  )}
-               </div>
-               <div className="flex -mt-9 flex-col justify-center items-center">
-                  <h2 className="font-medium text-174 text-gray-300">
-                     {data.data.consolidated_weather &&
-                        data.data.consolidated_weather[0].the_temp.toFixed(
-                           0
+         {data.data && (
+            <div>
+               {isMenuOpen ? (
+                  <HomeScreenLeftMenu setIsMenuOpen={setIsMenuOpen} />
+               ) : (
+                  <div className="px-4 md:px-1 lg:px-7 xl:px-10 2xl:px-14 pt-6  md:w-full z-0  lex flex-col overflow-x-hidden bg-purple100  min-h-screen">
+                     <header className="flex relative z-10 justify-between p-4">
+                        <input
+                           className="px-4 duration-200 cursor-pointer filter hover:bg-gray-700 outline-none py-2 shadow-md text-gray-50 bg-gray-500"
+                           onClick={() => setIsMenuOpen(true)}
+                           type="button"
+                           value="Search for places"
+                        />
+                        <MdGpsFixed
+                           color="white"
+                           onClick={() => setIsMenuOpen(true)}
+                           className="hover:bg-gray-700 duration-200 bg-gray-500 cursor-pointer text-gray-50 text-4xl p-2 box-border rounded-full"
+                        />
+                     </header>
+                     <div className="relative h-80 mt-4      ">
+                        <div className="w-bg absolute left-1/2 top-2/4 transform-translate">
+                           <img
+                              className="  opacity-10  w-bg   h-full    overflow-x-hidden  "
+                              src="/images/Cloud-background.png"
+                              alt=" a"
+                           />
+                        </div>
+                        {data.data.consolidated_weather && (
+                           <img
+                              className="absolute left-2/4 top-2/4 transform-translate  max-w-1 w-40"
+                              src={`/images/${data.data.consolidated_weather[0].weather_state_name.replace(
+                                 " ",
+                                 ""
+                              )}.png`}
+                              alt=""
+                           />
                         )}
-                     <span className="font-medium text-gray-400  text-5xl">
-                        °C
-                     </span>
-                  </h2>
-                  <h3 className="font-medium text-5xl text-gray-400">
-                     {data.data.consolidated_weather &&
-                        data.data.consolidated_weather[0]
-                           .weather_state_name}
-                  </h3>
-                  <span className="mt-16 text-xl text-gray-400">
-                     Today &nbsp;&nbsp;&nbsp;•{" "}
-                     <span>&nbsp;&nbsp;&nbsp; {date}</span>
-                  </span>
-                  <div className="flex justify-center mt-6 font-medium text-lg  text-gray-400 items-center">
-                     <MdPlace size="26" /> &nbsp;
-                     {data.data && data.data.title}
+                     </div>
+                     <div className="flex -mt-9 flex-col justify-center items-center">
+                        <h2 className="font-medium text-174 text-gray-300">
+                           {data.data.consolidated_weather &&
+                              data.data.consolidated_weather[0].the_temp.toFixed(
+                                 0
+                              )}
+                           <span className="font-medium text-gray-400  text-5xl">
+                              °C
+                           </span>
+                        </h2>
+                        <h3 className="font-medium text-5xl text-gray-400">
+                           {data.data.consolidated_weather &&
+                              data.data.consolidated_weather[0]
+                                 .weather_state_name}
+                        </h3>
+                        <span className="mt-16 text-xl text-gray-400">
+                           Today &nbsp;&nbsp;&nbsp;•{" "}
+                           <span>&nbsp;&nbsp;&nbsp; {date}</span>
+                        </span>
+                        <div className="flex justify-center mt-6 font-medium text-lg  text-gray-400 items-center">
+                           <MdPlace size="26" /> &nbsp;
+                           {data.data && data.data.title}
+                        </div>
+                     </div>
                   </div>
-               </div>
+               )}
             </div>
          )}
       </div>
--- src/components/HomeScreenRight.js
+++ src/components/HomeScreenRight.js
@@ -6,45 +6,48 @@ import SingleHightlights from "./SingleHightlights";
 const HomeScreenRight = ({ data }) => {
    const [date, setDate] = useState("");
    useEffect(() => {
+      console.log(data, "kd0psakd0sapkdsa09");
       setDate(format(new Date(), "yyyy-MM-d"));
    }, []);
    return (
       <div className="bg-purple200 ">
-         <div className=" min-h-screen mx-auto   px-3 max-w-6xl  2xl:pt-5 2xl:px-32   ">
-            <header className="flex justify-end items-center 2xl:py-0 py-8 2xl:pt-0 pr-10">
-               <p className="bg-gray-200 text-2xl rounded-full w-11 h-11  flex items-center mr-5 justify-center font-bold text-gray-900">
-                  °C
-               </p>
-               <p className="text-gray-200 rounded-full  flex items-center justify-center w-11 h-11 text-2xl bg-purple000 font-bold">
-                  °F
-               </p>
-            </header>
-            <div className="grid  lg:grid-cols-3 xl:grid-cols-5 lg:grid-rows-1  mt-6 text-gray-400 grid-cols-2 grid-rows-3 xl:gap-3 place-items-center 2xl:gap-4 gap-9 ">
-               {data.data.consolidated_weather &&
-                  data.data.consolidated_weather
-                     .filter((arrays) => {
-                        return arrays.applicable_date !== date;
-                     })
-                     .map((data) => {
-                        return (
-                           <SingleDays
-                              day={date}
-                              key={data.id}
-                              data={data}
-                           />
-                        );
-                     })}
+         {data.data && (
+            <div className=" min-h-screen mx-auto   px-3 max-w-6xl  2xl:pt-5 2xl:px-32   ">
+               <header className="flex justify-end items-center 2xl:py-0 py-8 2xl:pt-0 pr-10">
+                  <p className="bg-gray-200 text-2xl rounded-full w-11 h-11  flex items-center mr-5 justify-center font-bold text-gray-900">
+                     °C
+                  </p>
+                  <p className="text-gray-200 rounded-full  flex items-center justify-center w-11 h-11 text-2xl bg-purple000 font-bold">
+                     °F
+                  </p>
+               </header>
+               <div className="grid  lg:grid-cols-3 xl:grid-cols-5 lg:grid-rows-1  mt-6 text-gray-400 grid-cols-2 grid-rows-3 xl:gap-3 place-items-center 2xl:gap-4 gap-9 ">
+                  {data.data &&
+                     data.data.consolidated_weather
+                        .filter((arrays) => {
+                           return arrays.applicable_date !== date;
+                        })
+                        .map((data) => {
+                           return (
+                              <SingleDays
+                                 day={date}
+                                 key={data.id}
+                                 data={data}
+                              />
+                           );
+                        })}
+               </div>
+               <h2 className="text-gray-200 text-left font-medium  py-5  text-3xl mt-6">
+                  Today’s Hightlights{" "}
+               </h2>
+               <div className=" w-full grid grid-cols-1 grid-rows-1 md:grid-rows-2 md:ml-4 lg:ml-0 md:grid-cols-2 gap-5 2xl:gap-10   place-items-center  ">
+                  <SingleHightlights data={data} wind large />
+                  <SingleHightlights data={data} humidity large />
+                  <SingleHightlights data={data} visibility />
+                  <SingleHightlights data={data} air />
+               </div>
             </div>
-            <h2 className="text-gray-200 text-left font-medium  py-5  text-3xl mt-6">
-               Today’s Hightlights{" "}
-            </h2>
-            <div className=" w-full grid grid-cols-1 grid-rows-1 md:grid-rows-2 md:ml-4 lg:ml-0 md:grid-cols-2 gap-5 2xl:gap-10   place-items-center  ">
-               <SingleHightlights data={data} wind large />
-               <SingleHightlights data={data} humidity large />
-               <SingleHightlights data={data} visibility />
-               <SingleHightlights data={data} air />
-            </div>
-         </div>
+         )}
       </div>
    );
 };
